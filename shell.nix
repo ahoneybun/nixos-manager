4b573a9ea33a9b391aaa364198a43ce8eabec76e
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2022-06-06 17:00:03 -0600

    Replace ghc882 with ghc884

--- shell.nix
+++ shell.nix
@@ -2,4 +2,4 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
 in
-(import ./build.nix { inherit pkgs; compiler = "ghc882"; }).shell
+(import ./build.nix { inherit pkgs; compiler = "ghc884"; }).shell
